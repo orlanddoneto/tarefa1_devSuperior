7e75a751fa6be8168e719e425b39a1cb424117e2
Orlando Neto <neto020735@gmail.com>
2025-05-12 12:56:22 -0300

    Segundo ajuste para correção

--- src/main/java/com/orlando/tarefas/TarefasApplication.java
+++ src/main/java/com/orlando/tarefas/TarefasApplication.java
@@ -29,17 +29,21 @@ public class TarefasApplication  implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Integer codigo = sc.nextInt();
-
-		Double valorBase = sc.nextDouble();
-
-		Double desconto = sc.nextDouble();
-
-		Order pedido = new Order(codigo,valorBase,desconto);
-
-		System.out.println("Pedido Código: "+ pedido.getCode());
-		Double valorFinal = orderService.total(pedido) + shippingService.shipment(pedido);
-
+		Order pedido1 = new Order(1034,150.0,20.0);
+		System.out.println("Pedido Código: "+ pedido1.getCode());
+		Double valorFinal = orderService.total(pedido1) + shippingService.shipment(pedido1);
 		System.out.println("Valor Total: R$"+valorFinal);
+
+		Order pedido2 = new Order(2282,800.0,10.0);
+		System.out.println("Pedido Código: "+ pedido2.getCode());
+		Double valorFinal2 = orderService.total(pedido2) + shippingService.shipment(pedido2);
+		System.out.println("Valor Total: R$"+valorFinal2);
+
+		Order pedido3 = new Order(1309,95.90,0.0);
+		System.out.println("Pedido Código: "+ pedido3.getCode());
+		Double valorFinal3 = orderService.total(pedido3) + shippingService.shipment(pedido3);
+		System.out.println("Valor Total: R$"+valorFinal3);
+
+
 	}
 }
